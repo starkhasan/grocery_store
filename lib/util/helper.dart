b2e689202221265e313c413a4e6489ed8ef3463e
starkhasan <alihasan226@gmail.com>
2022-01-09 00:43:53 +0530

    Add Asset and Implement SplashScreen

--- lib/util/helper.dart
+++ lib/util/helper.dart
@@ -1,21 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/ui/deeplink/platform_integration_page.dart';
-import 'package:test_app/ui/facebook_page.dart';
-import 'package:test_app/ui/crypto_page.dart';
-import 'package:test_app/ui/get_started/get_started_home.dart';
-import 'package:test_app/ui/grocery/main_screen.dart';
-import 'package:test_app/ui/isolates_example.dart';
-import 'package:test_app/ui/keys_example/scroll_position_example.dart';
-import 'package:test_app/ui/keys_example/widget_element_keys.dart';
-import 'package:test_app/ui/payment_gateway/razorpay_payment.dart';
-import 'package:test_app/ui/rest_api.dart';
-import 'package:test_app/ui/twitter_page.dart';
-import 'package:test_app/ui/video_play_screen.dart';
-import 'package:test_app/ui/web_socket.dart';
-import 'package:test_app/ui/whatsapp_page.dart';
-import 'package:test_app/ui/webview_page.dart';
-import 'package:test_app/ui/sliverwidget_page.dart';
-import 'package:test_app/ui/firebase_authentication.dart';
 
 class Helper {
   
@@ -75,87 +58,5 @@ class Helper {
     'asset/whatsapp.png',
     'asset/crypto.png'
   ];
-
-  void landingPageNavigation(BuildContext context,int index){
-    switch (index) {
-      case 0:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const FacebookPage()));
-        break;
-      case 1:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const TwitterPage()));
-        break;
-      case 2:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const WhatsAppPage()));
-        break;
-      case 3:
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => const Crypto()));
-        break;
-      case 4:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const WebViewPage()));
-        break;
-      case 5:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const SliverWidgetPage()));
-        break;
-      case 6:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const FirebaseAuthentication()));
-        break;
-      case 7:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const RazorpayPaymentGateway()));
-        break;
-      case 8:
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (BuildContext context) {
-              return const PlatformIntegrationPage();
-            }
-          )
-        ).then((value) => {});
-        break;
-      case 9:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const RestAPI()));
-        break;
-      case 10:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const WebSocket()));
-        break;
-      case 11:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const WidgetElementKeys()));
-        break;
-      case 12:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const ScrollPositionExample()));
-        break;
-      case 13:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const VideoPlayScreen()));
-        break;
-      case 14:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const MainScreen()));
-        break;
-      case 15:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const IsolateExample()));
-        break;
-      case 16:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const GetStartedHome()));
-        break;
-      default:
-    }
-  }
-
   var groceryMoreItems = ['My Account','My Wishlist','Saved Addresses','All Orders','My Rewards'];
-
 }
-
-/**
- * Square
- * Sandbox Application ID
- * sandbox-sq0idb-SDo00t1mjESNdPC3dp1fXQ
- * Sandbox Access Token
- * EAAAELUlW3QKjLxy65AEG59yryCakHLl50uQSMOsovOHND84QbVa8P9XDHqW-fE0
- * Sandbox Location ID
- * LDWVWYVTV3AP7
- */
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:grocery_plus/view/main_screen.dart';
+import 'dart:async';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.green));
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
+    startTimer(context);
     return Scaffold(
-      appBar: AppBar(title: const Text('Splash Screen')),
-      body: const Center(child: Text('This is Splash Screen'))
+      body: Stack(
+        children: [
+          Container(color: const Color.fromARGB(255, 246, 246, 246),width: double.infinity,height: double.infinity,child: Align(alignment: Alignment.bottomRight,child: Image.asset('asset/grocery_background.jpg'))),
+           const Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Grocery Plus',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,fontFamily: '')
+            )
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, MediaQuery.of(context).size.height * 0.01),
+              child: const Text(
+                'Traversal',
+                style: TextStyle(color: Color.fromARGB(255, 201, 201, 201),fontSize: 11)
+              )
+            )
+          )
+        ]
+      )
     );
+  }
+
+  void startTimer(BuildContext _context){
+    Timer(const Duration(),() => Navigator.pushReplacement(_context, MaterialPageRoute(builder: (context) => const MainScreen())));
   }
 }
